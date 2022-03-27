fa7881a5aeaed3905632d935a24b59e69e7f4b43
Ashwin <ashwinrjprht@gmail.com>
2022-03-27 13:17:11 +0530

    UI is updated

---
+++ Assets/Scripts/Managers/levelloader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelloader : MonoBehaviour
+{
+    private void Awake()
+    {
+        //   nextsence();
+        Invoke("nextsence", 30);
+    }
+
+   void nextsence() 
+    { 
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
